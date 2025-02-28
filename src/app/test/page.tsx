a24f6e4d08df98e2a8a8eeb8fbe7787cc76fe139
Akshat <akshatmaurya25@gmail.com>
2025-02-28 13:49:01 +0530

    few tukings

---
+++ src/app/test/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import UploadFile from "@/utils/uploadFile";
+const page = () => {
+  return (
+    <div>
+      <UploadFile />
+    </div>
+  );
+};
+
+export default page;
--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -2,17 +2,17 @@
 import React, { useEffect, useState } from "react";
 import { Vortex } from "../ui/vortex";
 import { usePrivy } from "@privy-io/react-auth";
+
 export const Hero = () => {
- 
   const { ready, authenticated, login, user, logout } = usePrivy();
-  if(authenticated){
-    console.log(user)
+  if (authenticated) {
+    console.log(user);
   }
   // Disable login when Privy is not ready or the user is already authenticated
   const disableLogin = !ready || (ready && authenticated);
   return (
     <div className=" relative p-4 lg:p-8 mt-16">
-      <div className="h-[75vh] overflow-hidden w-full justify-center items-center flex flex-col  bg-black p-4 rounded-2xl">
+      {/* <div className="h-[75vh] overflow-hidden w-full justify-center items-center flex flex-col  bg-black p-4 rounded-2xl">
         <Vortex
           backgroundColor="black"
           rangeY={800}
@@ -24,20 +24,35 @@ export const Hero = () => {
             <h1>It all comes down to</h1>
           </div>
           <AnimatedNumberText />
-          <button disabled={disableLogin} onClick={login}>
-            Log in
-          </button>
-          <button disabled={!disableLogin} onClick={logout}>
-            Log out
-          </button>
         </Vortex>
+      </div> */}
+      <div className="relative w-full h-full">
+        <video
+          width="1920"
+          height="1080"
+          controls={false}
+          autoPlay
+          loop
+          muted
+          className="w-full h-full object-cover"
+        >
+          <source src="Air.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+
+        {/* Overlay Content */}
+        <div className="absolute gap-4 inset-0 flex flex-col items-center justify-center text-white montserrat">
+          <h1 className="text-4xl font-bold ">We bring the Numbers</h1>
+          <AnimatedNumberText />
+          {/* <h1 className="text-[7rem] ">DAO</h1> */}
+        </div>
       </div>
     </div>
   );
 };
 
 const AnimatedNumberText = () => {
-  const finalText = "NUMBERS";
+  const finalText = "NUMBERS DAO";
   const [displayText, setDisplayText] = useState("");
   const [isAnimating, setIsAnimating] = useState(true);
 
@@ -93,7 +108,7 @@ const AnimatedNumberText = () => {
 
   return (
     <div className="">
-      <div className=" text-[70px] lg:text-[100px] montserrat leading-none  font-bold text-whi font-mono">
+      <div className=" text-[70px] lg:text-[100px] grotesk leading-none  font-bold text-whi font-mono">
         {displayText || (isAnimating ? "0".repeat(finalText.length) : "")}
       </div>
     </div>
