1c21e8f523b4fa6fca7b1959e34d2bdae556357a
Pranesh <pranesh25joshi@gmail.com>
2025-03-08 16:39:42 +0530

    add images to CommunityImageGallery and update event details

--- src/app/community/communityimagegallary.tsx
+++ src/app/community/communityimagegallary.tsx
@@ -5,14 +5,43 @@ import { Navigation, Autoplay } from 'swiper/modules';
 import Image from 'next/image';
 import 'swiper/css';
 import 'swiper/css/navigation';
+import cimg1 from "@/assests/cimg1.jpg";
+import cimg2 from "@/assests/cimg2.jpg";
+import cimg3 from "@/assests/cimg3.jpg";
+import cimg4 from "@/assests/cimg4.jpg";
+import cimg5 from "@/assests/cimg5.jpg";
 
 const CommunityImageGallery = () => {
     const eventImages = [
         {
-            url: 'https://cdn.sanity.io/images/zg5gx8g4/production/1f26be1a64e998e09bc71356f0c15162cc148269-961x1280.jpg?w=961&h=1280&auto=format',
+            url: cimg5,
             title: 'Community Meetup 2023',
             description: 'Annual gathering of DAO members',
             date: '15 Dec 2023'
+        },
+        {
+            url: cimg3,
+            title: 'DAO Workshop',
+            description: 'Learning about Decentralization',
+            date: '10 Nov 2023'
+        },
+        {
+            url: cimg2,
+            title: 'Tech Conference',
+            description: 'Blockchain Technology Summit',
+            date: '5 Nov 2023'
+        },
+        {
+            url: cimg1,
+            title: 'Community Networking',
+            description: 'Building Connections in Web3',
+            date: '1 Nov 2023'
+        },
+        {
+            url: cimg4,
+            title: 'Innovation Meet',
+            description: 'Future of Web3 Discussion',
+            date: '20 Oct 2023'
         },
         {
             url: 'https://cdn.sanity.io/images/zg5gx8g4/production/3c75ad1fe0e408b33e0e5b130df105e3c19df782-2000x1334.jpg?w=2000&h=1334&auto=format',
@@ -21,34 +50,10 @@ const CommunityImageGallery = () => {
             date: '20 Nov 2023'
         },
         {
-            url: 'https://cdn.sanity.io/images/zg5gx8g4/production/527f0056d6a10a1f6f04495660c1a6ed84d5ee3c-4000x6000.jpg?w=2560&h=3840&auto=format',
-            title: 'DAO Workshop',
-            description: 'Learning about Decentralization',
-            date: '10 Nov 2023'
-        },
-        {
-            url: 'https://cdn.sanity.io/images/zg5gx8g4/production/451bcc4838f96174efe21a82eee7da7efedd32d3-2048x1536.jpg?w=2048&h=1536&auto=format',
-            title: 'Tech Conference',
-            description: 'Blockchain Technology Summit',
-            date: '5 Nov 2023'
-        },
-        {
-            url: 'https://cdn.sanity.io/images/zg5gx8g4/production/4d0708860d2263e3e111d123d8a01843e94eb17a-1179x783.png?w=1179&h=783&auto=format',
-            title: 'Community Networking',
-            description: 'Building Connections in Web3',
-            date: '1 Nov 2023'
-        },
-        {
             url: 'https://cdn.sanity.io/images/zg5gx8g4/production/09bb2857d0253d831cd25ed49584debbfbac6cd0-2195x1235.jpg?w=2195&h=1235&auto=format',
             title: 'DAO Governance',
             description: 'Decision Making Workshop',
             date: '25 Oct 2023'
-        },
-        {
-            url: 'https://cdn.sanity.io/images/zg5gx8g4/production/a8cdb7ac969870ad848cf41e7a495866b404c369-1350x1260.png?w=1350&h=1260&auto=format',
-            title: 'Innovation Meet',
-            description: 'Future of Web3 Discussion',
-            date: '20 Oct 2023'
         },
         {
             url: 'https://cdn.sanity.io/images/zg5gx8g4/production/527f0056d6a10a1f6f04495660c1a6ed84d5ee3c-4000x6000.jpg?w=2560&h=3840&auto=format',
